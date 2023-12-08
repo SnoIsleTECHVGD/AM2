b64ee946f533d0b67bcabe6f38b92ed80f538420
MUKILTEO\1600171 <1600171@mukilteo.wednet.edu>
2023-12-08 09:32:05 -0800

    Level progress

---
+++ Assets/Thomas/Scripts/Camerafollow.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Camerafollow : MonoBehaviour
+{
+    public Transform target;
+    public float speed;
+    public Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.position, Time.fixedDeltaTime * speed) + offset;
+    }
+}
