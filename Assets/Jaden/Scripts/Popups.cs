9a7105c2710476692388952732a6de665118842d
MUKILTEO\1717295 <1717295@mukilteo.wednet.edu>
2023-12-12 10:11:30 -0800

    Stuffs

--- Assets/Jaden/Scripts/Popups.cs
+++ Assets/Jaden/Scripts/Popups.cs
@@ -5,8 +5,17 @@ using UnityEngine.UI;
 
 public class Popups : MonoBehaviour
 {
-    public bool help;
-    public GameObject helpMe;
+    public bool isVisible1;
+    public bool isVisible2;
+    public bool isVisible3;
+    public bool isVisible4;
+    public bool isVisible5;
+    public GameObject Popup1;
+    public GameObject Popup2;
+    public GameObject Popup3;
+    public GameObject Popup4;
+    public GameObject Popup5;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,16 +25,56 @@ public class Popups : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(help == true)
+        if(isVisible1 == true)
         {
-            helpMe.SetActive(true);
-        } else
-        {
-            helpMe.SetActive(false);
+            Popup1.SetActive(true);
+            
         }
-        if (Input.GetKeyDown(KeyCode.P))
+        else
         {
-            help = !help;
+            Popup1.SetActive(false);
+            
         }
+        if (isVisible2 == true)
+        {
+            Popup2.SetActive(true);
+
+        }
+        else
+        {
+            Popup2.SetActive(false);
+
+        }
+        if (isVisible3 == true)
+        {
+            Popup3.SetActive(true);
+
+        }
+        else
+        {
+            Popup3.SetActive(false);
+
+        }
+        if (isVisible4 == true)
+        {
+            Popup4.SetActive(true);
+
+        }
+        else
+        {
+            Popup4.SetActive(false);
+
+        }
+        if (isVisible5 == true)
+        {
+            Popup5.SetActive(true);
+
+        }
+        else
+        {
+            Popup5.SetActive(false);
+
+        }
+         
     }
 }
