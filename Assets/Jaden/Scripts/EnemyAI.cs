57afed0ac9576dfca87ea724b6f52ab745c0a0a7
MUKILTEO\1717590 <1717590@apps.everettsd.org>
2024-01-19 09:08:20 -0800

    ez

--- Assets/Jaden/Scripts/EnemyAI.cs
+++ Assets/Jaden/Scripts/EnemyAI.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
-    /*public GameObject target;
+    public GameObject target;
     public GameObject weaponGrip;
 
     public float maxDetectionRange = 20f;
@@ -14,31 +14,25 @@ public class EnemyAI : MonoBehaviour
     private Transform targetTransform;
 
     private Stats myStats;
-    private Gun myGun;
+    public EnemyGun myGun;
 
-    // Start is called before the first frame update
     void Start()
     {
         myStats = GetComponent<Stats>();
-        myGun = myStats.gunWeapon;
 
         targetTransform = target.transform;
         targetStats = target.GetComponent<Stats>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (!target || targetStats.isDead == true)
-        {
-            return;
-        }
+        
 
         if (hasSeenPlayer == false)
         {
             myGun.StopShooting();
 
-            int layerMask = LayerMask.GetMask("Player", "Default");
+            int layerMask = LayerMask.GetMask("Player");
             RaycastHit2D cast = Physics2D.Raycast(transform.position, targetTransform.position - transform.position, maxDetectionRange, layerMask);
 
             if (cast.transform == targetTransform)
@@ -61,5 +55,5 @@ public class EnemyAI : MonoBehaviour
             myGun.StartShooting();
         }
     }
-    */
+    
 }
