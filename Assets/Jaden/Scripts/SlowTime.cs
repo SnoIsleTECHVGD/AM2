12759654be8aa4c0dc38124560862a379b6239ab
MUKILTEO\1717295 <1717295@mukilteo.wednet.edu>
2024-01-24 07:56:37 -0800

    hafhd

--- Assets/Jaden/Scripts/SlowTime.cs
+++ Assets/Jaden/Scripts/SlowTime.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SlowTime : MonoBehaviour
 {
     private bool time;
+    public float currentTime;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,8 +18,13 @@ public class SlowTime : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.F))
             time = !time;
         if (time)
+        {
+            if(Time.timeScale == 1)
             Time.timeScale = .5f;
+        }
         else
+            if(Time.timeScale == .5f)
             Time.timeScale = 1;
+        currentTime = Time.timeScale;
     }
 }
